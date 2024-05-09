28eee517cf6b3abfd065954baf3c31f84ae0b094
Maximilian Kemmer <kemmer.maximilian@gmail.com>
2024-05-09 17:56:19 +0200

    feat(FEAT-0015): implementation of return to main menu button

---
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    public GameObject confirmationDialog; // Reference to the confirmation dialog UI element
+
+    void Update()
+    {
+        // check if escape pressend and dialog is active
+        if (Input.GetKeyDown(KeyCode.Escape) && !confirmationDialog.activeSelf )
+        {
+            confirmationDialog.SetActive(true);
+        } else if (Input.GetKeyDown(KeyCode.Escape) && confirmationDialog.activeSelf)
+        {
+            CancelReturnToMainMenu();
+        }
+    }
+
+    public void ConfirmReturnToMainMenu()
+    {
+        Debug.Log("Returning to main menu");
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    private void CancelReturnToMainMenu()
+    {
+        Debug.Log("Returning to game");
+        confirmationDialog.SetActive(false);
+    }
+}
